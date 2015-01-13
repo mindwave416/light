6f52f9cb02b56075f6de6dab7067bf32356064bf
mindwave416 <bnaugle87@gmail.com>
2015-01-12 23:41:26 -0700

    TESTING

--- Light/AppDelegate.swift
+++ Light/AppDelegate.swift
@@ -22,6 +22,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window?.addSubview(lightSwitch)
         return true
     }
+    
+    /* BIG TEST COMMENT */
 
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
